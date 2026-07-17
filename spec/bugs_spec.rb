4fd83ab5dbfe5174c034a9ea9de8f7cfa71e3c87
Priyavadan Kumar <priyavadan.kumar@amd.com>
2026-07-17 11:28:16 -0500

    add support for unfixable bugs

--- spec/bugs_spec.rb
+++ spec/bugs_spec.rb
@@ -7,11 +7,18 @@ class BugsDut
   bug :low_vref, affected_version: 0
   bug :low_iref, affected_versions: [0, 1]
   bug :dac_code, fixed_on_version: 1
-  bug :unfixable
+  bug :bug1, unfixable: true
 
   def initialize(version)
     @version = version
   end
+end
+
+class UnversionedBugsDut
+  include Origen::Bugs
+
+  bug :my_bug, unfixable: true
+  bug :ordinary_bug
 end
 
 describe "Bugs API" do
@@ -31,7 +38,7 @@ describe "Bugs API" do
     $dut.has_bug?(:low_vref).should == true
     $dut.has_bug?(:low_iref).should == true
     $dut.has_bug?(:dac_code).should == true
-    $dut.has_bug?(:unfixable).should == true
+    $dut.has_bug?(:bug1).should == true
     $dut.has_bug?(:undefined).should == false
   end
 
@@ -39,7 +46,14 @@ describe "Bugs API" do
     Origen.load_target("configurable", dut: BugsDut, version: 0)
     $dut.bugs[:low_iref].fixed_on_version.should == 2
     $dut.bugs[:dac_code].fixed_on_version.should == 1
-    $dut.bugs[:unfixable].fixed_on_version.should == nil
+    $dut.bugs[:bug1].fixed_on_version.should == nil
+  end
+
+  it "explicitly unfixable bugs can be queried without a version" do
+    dut = UnversionedBugsDut.new
+    dut.bugs[:my_bug].unfixable?.should == true
+    dut.has_bug?(:my_bug).should == true
+    lambda { dut.has_bug?(:ordinary_bug) }.should raise_error(RuntimeError, 'Version undefined!')
   end
 
   it "bug presence methods scope to the current version" do
@@ -47,12 +61,12 @@ describe "Bugs API" do
     $dut.has_bug?(:low_vref).should == false
     $dut.has_bug?(:low_iref).should == true
     $dut.has_bug?(:dac_code).should == false
-    $dut.has_bug?(:unfixable).should == true
+    $dut.has_bug?(:bug1).should == true
     Origen.load_target("configurable", dut: BugsDut, version: 2)
     $dut.has_bug?(:low_vref).should == false
     $dut.has_bug?(:low_iref).should == false
     $dut.has_bug?(:dac_code).should == false
-    $dut.has_bug?(:unfixable).should == true
+    $dut.has_bug?(:bug1).should == true
   end
 
 end
